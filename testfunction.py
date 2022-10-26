39961b9b6c5003e973d7aedfa348a0979ccf7e68
samerboukaram <boukaramsamer@gmail.com>
2022-10-26 16:35:21 +0300

    repository cleaned

--- testfunction.py
+++
@@ -1,7 +0,0 @@
-def poop():
-    print("this is poop")
-
-
-def peep(continent='Asia'):
-        print('The name of continent is : ', continent)
-
--- testserialrc copy.py
+++
@@ -1,8 +0,0 @@
-
-import serial
-
-ser = serial.Serial(port="/dev/serial0", baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-
-while True:
-    data_buf = ser.read(16)
-    data = data_buf[2:]
